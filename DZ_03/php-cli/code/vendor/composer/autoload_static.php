61b89b507eb296f7074e2a1019c5a6257b156f2e
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-08-28 18:00:04 +0300

    DZ_03 1/2 done!

--- DZ_03/php-cli/code/vendor/composer/autoload_static.php
+++ DZ_03/php-cli/code/vendor/composer/autoload_static.php
@@ -10,7 +10,7 @@ class ComposerStaticInitcda29c6859a2ec15daef0739cabebd88
         '73a76277cdd754516bf94aa9c0aa6bb3' => __DIR__ . '/../..' . '/src/main.function.php',
         '2c30778c83e7cf1ab5d05f6fb053a212' => __DIR__ . '/../..' . '/src/template.function.php',
         '4497162affd5dbda9202a35ac3a5f40d' => __DIR__ . '/../..' . '/src/file.function.php',
-        '180746bbc1e8c796382f0fdeb325f75f' => __DIR__ . '/../..' . '/src/data1.function.php',
+        'da2b492aeaabe8950c9aa255152c0900' => __DIR__ . '/../..' . '/src/dataNew.function.php',
     );
 
     public static $classMap = array (
